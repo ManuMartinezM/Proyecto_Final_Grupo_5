0083880abde23f493df1ef2f539f57b4602500ad
Norman2022 <normanmaciel@gmail.com>
2023-11-02 16:24:39 -0300

    add files

--- SCRIPT/download_s3.py
+++ SCRIPT/download_s3.py
@@ -11,22 +11,17 @@ session = Session(aws_access_key_id=AWS_ACCESS_KEY_ID,
                 aws_secret_access_key=AWS_SECRET_ACCESS_KEY)
 
 def download_file(bucket_name, bucket_path, dest_path):
-
     print(f"Downloading file...")
     
     s3 = session.resource('s3')
     bucket = s3.Bucket(bucket_name)
     bucket.download_file(bucket_path, dest_path)
-
     print(f"Download File {bucket_path} at {dest_path}")
 
 
 def get_files_objects(bucket_name, bucket_folder):
 
-   
-
     all_objs = s3.list_objects_v2(Bucket=bucket_name, Prefix=f"{bucket_folder}", Delimiter="/")
-
     files_objs = []
 
     for obj in all_objs['Contents']:
@@ -51,8 +46,6 @@ def download_all_files(bucket_name, bucket_folder):
 
 if __name__ == '__main__':
     bucket_name = BUCKET_NAME
-    out_folder = 'EDA'
-    #file_path = "clean_data/clean_taxi_zone.csv"
-    #out_path = "EDA/totals-watch-time.csv"
-    
-    download_all_files(bucket_name, "clean_data/")
+    out_folder = 'EDA' #local destination folder
+       
+    download_all_files(bucket_name, "clean_data/")# S3 folder

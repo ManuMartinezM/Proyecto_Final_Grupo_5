80657851ddffab88ec40e86a3a12456276e07b00
ManuMartinezM <manuelmartinezmargalef@gmail.com>
2023-11-02 21:53:47 -0300

    Streamlit

---
+++ Streamlit/dashboard.py
@@ -0,0 +1,16 @@
+import streamlit as st
+import plotly.express as px
+import pandas as pd
+import os
+import warnings
+
+warnings.filterwarnings('ignore')
+
+st.set_page_config(page_title="Final Project - HENRY (Group 5)", page_icon=":taxi:", layout="wide")
+
+st.title(":bar_chart: HENRY: Final Project - Group 5")
+st.markdown("***")
+
+st.image("Cover.jpg", caption="SmartANALYTICS")
+
+

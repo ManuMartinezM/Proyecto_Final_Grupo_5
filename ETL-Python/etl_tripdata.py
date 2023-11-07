bf1dbb8502f90e11981585087fbc6e60ed9813a4
daimena <daiana.mena@gmail.com>
2023-11-07 12:31:12 -0300

    Use vectorized datetime functions

--- ETL-Python/etl_tripdata.py
+++ ETL-Python/etl_tripdata.py
@@ -1,5 +1,4 @@
 import pandas as pd
-from datetime import datetime
 import argparse
 
 
@@ -8,21 +7,18 @@ def clean_df(df, service_type):
 
     df.dropna(how='any', inplace=True)
 
-    df['year'] = df.apply(
-        lambda f: get_year(str(f['pickup_datetime'])), axis=1)
-    df['month'] = df.apply(
-        lambda f: get_month(str(f['pickup_datetime'])), axis=1)
-    df['day'] = df.apply(
-        lambda f: get_day(str(f['pickup_datetime'])), axis=1)
-    df['pu_time'] = df.apply(
-        lambda f: get_PU_time(str(f['pickup_datetime'])), axis=1)
-    df['do_time'] = df.apply(
-        lambda f: get_DO_time(str(f['dropoff_datetime'])), axis=1)
+    df['year'] = df['pickup_datetime'].dt.year
+    df['month'] = df['pickup_datetime'].dt.month
+    df['day'] = df['pickup_datetime'].dt.day
+
+    hours = (df['pickup_datetime'].dt.hour).astype(str).str.zfill(2)
+    minutes = (df['pickup_datetime'].dt.minute).astype(str).str.zfill(2)
+
+    df['pu_time'] = hours.str.cat(minutes, sep=':')
 
     if not 'trip_time' in df.columns:
         # Some service types do not include the trip_time field so we need to compute it
-        df['trip_time'] = df.apply(lambda f: get_triptime(
-            str(f['pickup_datetime']), str(f['dropoff_datetime'])), axis=1)
+        df['trip_time'] = df['dropoff_datetime'] - df['pickup_datetime']
 
     df['type_service'] = 1 if service_type == 'fhvhv' else 0  # 0 stands for 'not for hire vehicle'
 
@@ -72,33 +68,6 @@ def standardize_df_fields(df, service_type):
 
     df.rename(mapper={PU_DATETIME_FIELDNAME: 'pickup_datetime', DO_DATETIME_FIELDNAME: 'dropoff_datetime',
               TRIP_DISTANCE_FIELDNAME: 'trip_distance', TOTAL_AMOUNT_FIELDNAME: 'total_amount'}, axis=1, inplace=True)
-
-
-def get_year(pu_iso_datetime):
-    return datetime.fromisoformat(pu_iso_datetime).year
-
-
-def get_month(pu_iso_datetime):
-    return datetime.fromisoformat(pu_iso_datetime).month
-
-
-def get_day(pu_iso_datetime):
-    return datetime.fromisoformat(pu_iso_datetime).day
-
-
-def get_PU_time(pu_iso_datetime):
-    return datetime.fromisoformat(pu_iso_datetime).strftime('%H:%M')
-
-
-def get_DO_time(do_iso_datetime):
-    return datetime.fromisoformat(do_iso_datetime).strftime('%H:%M')
-
-
-def get_triptime(pu_iso_datetime, do_iso_datetime):
-    pu = datetime.fromisoformat(pu_iso_datetime)
-    do = datetime.fromisoformat(do_iso_datetime)
-    triptime = do - pu
-    return int(triptime.total_seconds())
 
 
 if __name__ == '__main__':

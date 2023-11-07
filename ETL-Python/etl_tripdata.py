70ca50522f7705a3acd0b2aadcac24eff558b537
daimena <daiana.mena@gmail.com>
2023-11-07 19:31:45 -0300

    added outlier management to etl

--- ETL-Python/etl_tripdata.py
+++ ETL-Python/etl_tripdata.py
@@ -22,14 +22,28 @@ def clean_df(df, service_type):
 
     df['type_service'] = 1 if service_type == 'fhvhv' else 0  # 0 stands for 'not for hire vehicle'
 
-    df['DOLocationID'].replace(57, 56, inplace=True)
-    df['PULocationID'].replace(57, 56, inplace=True)
-    df['PULocationID'].replace([104, 105], 103, inplace=True)
-    df['DOLocationID'].replace([104, 105], 103, inplace=True)
+    df = do_zone_replacements(df)
 
     # We no longer need the datetimes since we've extracted them into year, month, day and time
     to_drop = ['pickup_datetime', 'dropoff_datetime']
     df.drop(columns=to_drop, inplace=True)
+
+    df.drop(df[df['DOLocationID'] == 264].index, inplace=True)
+    df.drop(df[df['DOLocationID'] == 265].index, inplace=True)
+    df.drop(df[df['PULocationID'] == 264].index, inplace=True)
+    df.drop(df[df['PULocationID'] == 265].index, inplace=True)
+
+    # Outlier management:
+    df.drop(df[df['total_amount'] <= 0].index, inplace=True)
+
+    df.drop(df[df['trip_time'] <= 0].index, inplace=True)
+    df.drop(df[df['trip_distance'] > 2000].index, inplace=True)
+    df['trip_distance'].replace([0], 3.18, inplace=True)
+
+    long_trips = df.loc[df['trip_time'] > 6000]
+    triptime_outliers = (long_trips.loc[long_trips['total_amount'] < 10]).index
+    triptime_outliers.append((df.loc[df['trip_time'] > 9000]).index)
+    df.drop(index=triptime_outliers, inplace=True)
 
 
 def standardize_df_fields(df, service_type):
@@ -73,6 +87,26 @@ def standardize_df_fields(df, service_type):
 
     df.rename(mapper={PU_DATETIME_FIELDNAME: 'pickup_datetime', DO_DATETIME_FIELDNAME: 'dropoff_datetime',
               TRIP_DISTANCE_FIELDNAME: 'trip_distance', TOTAL_AMOUNT_FIELDNAME: 'total_amount'}, axis=1, inplace=True)
+
+
+def do_zone_replacements(df):
+    # This is an id for a non-existent zone, it is replaced with id 56 which corresponds to the correct zone
+    DELETED_ZONES_A = [57]
+    REPLACEMENT_ZONE_A = 56
+
+    # These are three zones with different IDs that are actually all the same zone. We combine all three into id 103
+    DELETED_ZONES_B = [104, 105]
+    REPLACEMENT_ZONE_B = 103
+
+    zone_sets = [
+        (DELETED_ZONES_A, REPLACEMENT_ZONE_A), (DELETED_ZONES_B, REPLACEMENT_ZONE_B)
+    ]
+
+    for (deleted_zones, replacement_zone) in zone_sets:
+        df['PULocationID'].replace(deleted_zones, replacement_zone, inplace=True)
+        df['DOLocationID'].replace(deleted_zones, replacement_zone, inplace=True)
+
+    return df
 
 
 if __name__ == '__main__':

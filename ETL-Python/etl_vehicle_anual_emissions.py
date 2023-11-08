5b2606578e19877d5e0b4b24b8c7759517106699
MTPliauzer <trini.pliauzer@gmail.com>
2023-11-08 15:17:40 -0300

    etl

--- ETL-Python/etl_vehicle_anual_emissions.py
+++
@@ -1,5 +0,0 @@
-import pandas as pd
-
-df=pd.read_csv('Datasets/file.csv')
-df[['Year','Brand','Model','Fuel']]=df['vehicle'].str.extract('(\d{4}) (Land Rover|(?:\w+-\w+)|(?:\w+)) (.*) (EV|Gasoline|Hybrid)', expand=True)
-df.drop(columns=['vehicle'], inplace=True)

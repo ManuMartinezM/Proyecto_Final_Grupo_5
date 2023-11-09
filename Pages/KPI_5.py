7b80e62e8ba293cce9e760364b044c1412f2c6bc
ManuMartinezM <manuelmartinezmargalef@gmail.com>
2023-11-09 18:04:04 -0300

    dashboard

--- Pages/KPI_5.py
+++ Pages/KPI_5.py
@@ -25,8 +25,7 @@ def display_KPI_5_page():
         s3_staging_dir=athena_s3_staging_dir,
         schema_name=athena_database,
         region_name=aws_region
-    ) 
-    
+    )     
 
     # Write SQL query to calculate average emissions for EVs and hybrids
     kpi_query = """
@@ -83,12 +82,6 @@ def display_KPI_5_page():
     else:
         st.error(f'Potential reduction in emissions: {emission_reduction:.2f}%')
 
-    # Add a sidebar filter for selecting the year
-    selected_year = st.sidebar.selectbox("Select Year", ["2022", "2023", "Both"], index=2)
-
-    # Create a filter for the user to choose between "EV," "Hybrid," "Gasoline," or view all by default
-    selected_fuel_type = st.sidebar.selectbox("Select Fuel Type", ["EV", "Hybrid", "Gasoline", "All"], index=3)
-
     # Write SQL query to retrieve emissions data for EVs, hybrids, and gasoline vehicles
     query_1 = """
     SELECT
@@ -111,7 +104,7 @@ def display_KPI_5_page():
         df = pd.DataFrame(results, columns=columns)
 
     # Create a custom color palette
-    color_palette = ['#ADD8E6', '#90EE90', '#FFA07A']
+    color_palette = {'Gasoline': '#FFA07A', 'EV': '#90EE90', 'Hybrid': '#ADD8E6'}
 
     # Define a consistent figure size
     fig_width = 350
@@ -120,28 +113,20 @@ def display_KPI_5_page():
     # Create columns to display figures and titles side by side
     col1, col2, col3 = st.columns(3)
 
-    # Check if the selected year is "Both" or exists in the DataFrame
-    if selected_year == "Both" or str(selected_year) in df['Year'].unique().astype(str):
-        # Filter DataFrame based on the selected year
-        if selected_year != "Both":
-            df = df[df['Year'] == str(selected_year)]
-
         # Create a stacked bar chart using Plotly
-        fig_1 = go.Figure()
+    fig_1 = go.Figure()
 
-        for fuel_type in df['Fuel'].unique():
-            filtered_fuel_df = df[df['Fuel'] == fuel_type]
-            fig_1.add_trace(go.Bar(x=filtered_fuel_df['Year'], y=filtered_fuel_df['total_emissions'], name=fuel_type, marker_color=color_palette[df['Fuel'].unique().tolist().index(fuel_type)]))
+    for fuel_type in df['Fuel'].unique():
+        filtered_fuel_df = df[df['Fuel'] == fuel_type]
+        fig_1.add_trace(go.Bar(x=filtered_fuel_df['Year'], y=filtered_fuel_df['total_emissions'], name=fuel_type, marker_color=color_palette.get(fuel_type, 'gray')))
 
-        fig_1.update_layout(title_text='Annual Carbon Emissions', width=fig_width, height=fig_height)
-        fig_1.update_xaxes(title_text='Year')
-        fig_1.update_yaxes(title_text='Total Emissions (lbs CO2)')
+    fig_1.update_layout(title_text='Annual Carbon Emissions', width=fig_width, height=fig_height)
+    fig_1.update_xaxes(title_text='Year')
+    fig_1.update_yaxes(title_text='Total Emissions (lbs CO2)')
 
-        # Display the chart in the Streamlit app
-        col1.plotly_chart(fig_1)
-    else:
-        # Handle the case when the selected year doesn't exist in the DataFrame
-        col1.write(f"No data available for the year {selected_year}")
+    # Display the chart in the Streamlit app
+    col1.plotly_chart(fig_1)
+
 
     # Write SQL query to retrieve average annual emissions for each type of fuel
     query_2 = """
@@ -163,15 +148,67 @@ def display_KPI_5_page():
         columns = [desc[0] for desc in cursor.description]
         df_avg_emissions = pd.DataFrame(results, columns=columns)
 
-    # Filter the DataFrame based on the selected fuel type
-    if selected_fuel_type != "All":
-        df_avg_emissions = df_avg_emissions[df_avg_emissions['Fuel'] == selected_fuel_type]
 
     # Create a pie chart using Plotly Go to show the distribution of average annual carbon emissions for each fuel type
-    fig_2 = go.Figure(data=[go.Pie(labels=df_avg_emissions['Fuel'], values=df_avg_emissions['avg_emissions'], marker_colors=color_palette)])
+    fig_2 = go.Figure(data=[go.Pie(labels=df_avg_emissions['Fuel'], values=df_avg_emissions['avg_emissions'], marker_colors=[color_palette.get(fuel_type, 'gray') for fuel_type in df_avg_emissions['Fuel']])])
     fig_2.update_layout(title_text='Distribution of Average Annual Carbon Emissions by Fuel Type', width=fig_width, height=fig_height)
 
     # Display the pie chart in the Streamlit app
     col2.plotly_chart(fig_2)
+
+   # Create a radio button for the user to choose between "EV," "Hybrid," "Gasoline," with "EV" selected by default
+    selected_fuel_type = col3.radio("Select Fuel Type", ["EV", "Hybrid", "Gasoline"], index=0, horizontal=True)
+
+    # Create a radio button for the user to choose the year, with "Both" selected by default
+    selected_year_radio = col3.radio("Select Year", ["2022", "2023"], index=1, horizontal=True)
+
+    # Write SQL query to retrieve emissions data for the top 5 vehicle models with the least emissions based on the selected fuel type
+    # Update the SQL query to use the selected year from the radio button
+
+    query_3 = f"""
+        SELECT
+            Model,
+            Annual_emissions_lbs_co2
+        FROM
+            annual_vehicle_emissions
+        WHERE
+            Fuel = '{selected_fuel_type}' AND CAST(Year AS varchar) IN ('{selected_year_radio}', 'Both')
+        ORDER BY
+            Annual_emissions_lbs_co2
+        LIMIT 5;
+    """
+
+    # Execute the SQL query and retrieve the results as a DataFrame
+    with conn.cursor() as cursor:
+        cursor.execute(query_3)
+        results = cursor.fetchall()
+        columns = [desc[0] for desc in cursor.description]
+        df_top_models = pd.DataFrame(results, columns=columns)
+
+    # Create a bar chart using Plotly Go for the top 5 vehicle models with the least emissions
+    fig_3 = go.Figure()
+
+    fig_3.add_trace(go.Bar(
+        x=df_top_models['Model'],
+        y=df_top_models['Annual_emissions_lbs_co2'],
+        text=df_top_models['Annual_emissions_lbs_co2'],
+        textposition='auto',
+        marker=dict(color=color_palette.get(selected_fuel_type, 'gray'))  # Use the color of the selected fuel type
+    ))
+
+    # Customize the layout of the bar chart
+    fig_3.update_layout(
+        title=f'Top 5 {selected_fuel_type} Models with Least Annual Emissions (CO2)',
+        xaxis=dict(title='Model'),
+        yaxis=dict(title='Annual Emissions (lbs CO2)'),
+        width=fig_width,
+        height=fig_height
+    )
+
+    # Display the radio button and the bar chart in the Streamlit app
+    col3.plotly_chart(fig_3)
+
+
+
 
 
